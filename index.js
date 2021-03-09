5b49d085cbf56f4145f17f5f27751a89c79530ce
osxgirl <osxgirl@mac.com>
2021-03-09 11:28:46 -0500

    commit tree 6bb7be1054d6c86d08258551dd8d3d8b315cf939

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^a-z' -]/ig, "")
+  }
+  static titleize(string){
+    let str = Formatter.capitalize(string)
+    str = str.split(' ')
+    let ans = str[0]
+    for(let i = 1; i < str.length; i++){
+      if (str[i] == 'the'||
+          str[i] == 'a'||
+          str[i] == 'an'||
+          str[i] == 'but'||
+          str[i] == 'of'||
+          str[i] == 'and'||
+          str[i] == 'for'||
+          str[i] == 'at'||
+          str[i] == 'by'||
+          str[i] == 'from'){
+        ans += " " + str[i]
+      }
+      else{
+        ans += " " + Formatter.capitalize(str[i])
+      }
+    }
+    return ans
+  }
 }
